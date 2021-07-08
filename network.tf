e1a679cd44c4d30ac896674a0fc590c736d34caa
Eric Muir <eric@ebdm.dev>
2021-07-08 08:53:55 +0100

    Added correct network ingress

--- network.tf
+++ network.tf
@@ -26,6 +26,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["${var.ingress_ip}/32"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["::/0", "0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
