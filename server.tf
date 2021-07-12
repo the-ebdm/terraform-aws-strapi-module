30cce53ffdcf529e2d982c813a2163be65fb8bb7
Eric Muir <eric@ebdm.dev>
2021-07-12 14:39:04 +0100

    Rearrange steps

--- server.tf
+++ server.tf
@@ -38,8 +38,8 @@ unzip /home/ubuntu/strapi-${var.id}.zip -d /home/ubuntu/strapi
 chown ubuntu:ubuntu -R /home/ubuntu/strapi
 
 su ubuntu -c "cd ~/strapi && npm install && NODE_ENV=production npm run build"
-su ubuntu -c "/home/ubuntu/.npm-global/bin/pm2 start ~/strapi/ecosystem.config.js"
 su ubuntu -c "aws s3 cp s3://franscape-data-archive/${var.id}.db /home/ubuntu/strapi/.tmp/data.db"
+su ubuntu -c "/home/ubuntu/.npm-global/bin/pm2 start ~/strapi/ecosystem.config.js"
 
 echo "*/5 * * * * root /bin/sh aws s3 cp /home/ubuntu/strapi/.tmp/data.db s3://franscape-data-archive/${var.id}.db" >> /etc/cron.d/backup_database
 EOF
